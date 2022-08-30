c3ca8a8188aa3951173fa0773bd004b067c58ccc
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2022-08-30 18:13:16 +0100

    Slider project done in create-react-app

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// I included an alternative slider here and left the other one in the src folder marked as App.js
+import Alternative from './alt-slider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Alternative />
   </React.StrictMode>,
   document.getElementById('root')
 );
